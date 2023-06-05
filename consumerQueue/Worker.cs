cb59c5b719b5b5e777d190b247b4326119290fe5
claudiogodoy99 <cavalcantigodoy@gmail.com>
2023-06-05 15:10:05 -0300

    Tava zuado

--- consumerQueue/Worker.cs
+++ consumerQueue/Worker.cs
@@ -22,9 +22,58 @@ public class Worker : BackgroundService
         return delay.TotalMilliseconds;
     }
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        Task.Run(() => {
+        Task consumeFromEventHub = Task.Run(() =>
+       {
+           var appSettings = _config.GetSection("AppSettings");
+           var brokerList = appSettings["BrokerList"];
+           var topic = appSettings["Topic"];
+           var config = new ConsumerConfig
+           {
+               BootstrapServers = brokerList,
+               SecurityProtocol = SecurityProtocol.SaslSsl,
+               SocketTimeoutMs = 60000,                //this corresponds to the Consumer config `request.timeout.ms`
+               SessionTimeoutMs = 30000,
+               SaslMechanism = SaslMechanism.Plain,
+               SaslUsername = "$ConnectionString",
+               SaslPassword = _config.GetConnectionString("EventHub"),
+               GroupId = Guid.NewGuid().ToString(),
+               AutoOffsetReset = AutoOffsetReset.Latest,
+               EnableAutoCommit = true,
+               MaxPartitionFetchBytes = 1048576,
+               FetchWaitMaxMs = 10,
+               BrokerVersionFallback = "1.0.0",        //Event Hubs for Kafka Ecosystems supports Kafka v1.0+, a fallback to an older API will fail
+                                                       //Debug = "security,broker,protocol"    //Uncomment for librdkafka debugging information
+           };
+           using var consumer = new ConsumerBuilder<int, string>(config).SetKeyDeserializer(Deserializers.Int32).SetValueDeserializer(Deserializers.Utf8).Build();
+
+           consumer.Subscribe(topic);
+           _logger.LogInformation("Consuming messages from topic: {topic}, broker(s): {brokerList}", topic, brokerList);
+
+           ConsumeResult<int, string>? msg = null;
+
+           while (!stoppingToken.IsCancellationRequested)
+           {
+               try
+               {
+                   msg = consumer.Consume(10);
+                   if (msg == null) continue;
+
+                   concurrentQueue.Enqueue(msg);
+               }
+               catch (ConsumeException e)
+               {
+                   _logger.LogError(e, "Consume error");
+               }
+               catch (OperationCanceledException)
+               {
+               }
+           }
+       }, stoppingToken);
+
+        Task consumeFromQueue = Task.Run(() =>
+        {
             long count = 0;
             double min = double.MaxValue;
             double max = double.MinValue;
@@ -37,7 +86,7 @@ public class Worker : BackgroundService
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                while (!concurrentQueue.TryDequeue(out msg)) {}
+                while (!concurrentQueue.TryDequeue(out msg)) { }
 
                 if (msg == null) continue;
 
@@ -54,52 +103,6 @@ public class Worker : BackgroundService
 
         }, stoppingToken);
 
-        return Task.Run(() =>
-        {
-            var appSettings = _config.GetSection("AppSettings");
-            var brokerList = appSettings["BrokerList"];
-            var topic = appSettings["Topic"];
-            var config = new ConsumerConfig
-            {
-                BootstrapServers = brokerList,
-                SecurityProtocol = SecurityProtocol.SaslSsl,
-                SocketTimeoutMs = 60000,                //this corresponds to the Consumer config `request.timeout.ms`
-                SessionTimeoutMs = 30000,
-                SaslMechanism = SaslMechanism.Plain,
-                SaslUsername = "$ConnectionString",
-                SaslPassword = _config.GetConnectionString("EventHub"),
-                GroupId = Guid.NewGuid().ToString(),
-                AutoOffsetReset = AutoOffsetReset.Latest,
-                EnableAutoCommit = true,
-                MaxPartitionFetchBytes = 1048576,
-                FetchWaitMaxMs = 10,
-                BrokerVersionFallback = "1.0.0",        //Event Hubs for Kafka Ecosystems supports Kafka v1.0+, a fallback to an older API will fail
-                                                        //Debug = "security,broker,protocol"    //Uncomment for librdkafka debugging information
-            };
-            using var consumer = new ConsumerBuilder<int, string>(config).SetKeyDeserializer(Deserializers.Int32).SetValueDeserializer(Deserializers.Utf8).Build();
-
-            consumer.Subscribe(topic);
-            _logger.LogInformation("Consuming messages from topic: {topic}, broker(s): {brokerList}", topic, brokerList);
-
-            ConsumeResult<int, string>? msg = null;
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    msg = consumer.Consume(10);
-                    if (msg == null) continue;
-
-                    concurrentQueue.Enqueue(msg);
-                }
-                catch (ConsumeException e)
-                {
-                    _logger.LogError(e, "Consume error");
-                }
-                catch (OperationCanceledException)
-                {
-                }
-            }
-        }, stoppingToken);
+        await Task.WhenAll(consumeFromEventHub, consumeFromQueue);
     }
 }
