6b91c8001436b0a8bf4abd4e6bab49069e57ebd7
claudiogodoy99 <cavalcantigodoy@gmail.com>
2023-06-05 14:55:43 -0300

    new way to consume messages

---
+++ consumerQueue/Program.cs
@@ -0,0 +1,10 @@
+using consumerQueue;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
---
+++ consumerQueue/Worker.cs
@@ -0,0 +1,105 @@
+using Confluent.Kafka;
+using System.Collections.Concurrent;
+
+namespace consumerQueue;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _config;
+
+    private readonly ConcurrentQueue<ConsumeResult<int, string>> concurrentQueue = new ConcurrentQueue<ConsumeResult<int, string>>();
+
+    public Worker(ILogger<Worker> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    private double CalculateDelay(Timestamp ts)
+    {
+        var delay = DateTimeOffset.UtcNow - DateTimeOffset.FromUnixTimeMilliseconds(ts.UnixTimestampMs);
+        return delay.TotalMilliseconds;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Task.Run(() => {
+            long count = 0;
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            double cur = 0;
+            double avg = 0;
+
+            ConsumeResult<int, string>? msg = null;
+
+            _logger.LogInformation("count, cur, min, max, avg");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                while (!concurrentQueue.TryDequeue(out msg)) {}
+
+                if (msg == null) continue;
+
+                cur = CalculateDelay(msg.Message.Timestamp);
+                count++;
+                if (cur < min) min = cur;
+                if (cur > max) max = cur;
+                avg = ((count - 1) * avg + cur) / count;
+                if (count % 1000 == 0)
+                {
+                    _logger.LogInformation("{count}, {cur}, {min}, {max}, {avg}", count, cur, min, max, avg);
+                }
+            }
+
+        }, stoppingToken);
+
+        return Task.Run(() =>
+        {
+            var appSettings = _config.GetSection("AppSettings");
+            var brokerList = appSettings["BrokerList"];
+            var topic = appSettings["Topic"];
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = brokerList,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SocketTimeoutMs = 60000,                //this corresponds to the Consumer config `request.timeout.ms`
+                SessionTimeoutMs = 30000,
+                SaslMechanism = SaslMechanism.Plain,
+                SaslUsername = "$ConnectionString",
+                SaslPassword = _config.GetConnectionString("EventHub"),
+                GroupId = Guid.NewGuid().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                EnableAutoCommit = true,
+                MaxPartitionFetchBytes = 1048576,
+                FetchWaitMaxMs = 10,
+                BrokerVersionFallback = "1.0.0",        //Event Hubs for Kafka Ecosystems supports Kafka v1.0+, a fallback to an older API will fail
+                                                        //Debug = "security,broker,protocol"    //Uncomment for librdkafka debugging information
+            };
+            using var consumer = new ConsumerBuilder<int, string>(config).SetKeyDeserializer(Deserializers.Int32).SetValueDeserializer(Deserializers.Utf8).Build();
+
+            consumer.Subscribe(topic);
+            _logger.LogInformation("Consuming messages from topic: {topic}, broker(s): {brokerList}", topic, brokerList);
+
+            ConsumeResult<int, string>? msg = null;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    msg = consumer.Consume(10);
+                    if (msg == null) continue;
+
+                    concurrentQueue.Enqueue(msg);
+                }
+                catch (ConsumeException e)
+                {
+                    _logger.LogError(e, "Consume error");
+                }
+                catch (OperationCanceledException)
+                {
+                }
+            }
+        }, stoppingToken);
+    }
+}
